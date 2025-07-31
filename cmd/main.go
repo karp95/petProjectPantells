1e54c949493be08ebbfa8638c09694bb9b1cd4e2
Dmitriy <swagkarpavg@gmail.com>
2025-07-31 03:27:09 +0300

    Ручки GET и POST

---
+++ cmd/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
+
+type RequestBody struct {
+	Task string `json:"task"`
+}
+
+var task = RequestBody{
+	Task: "Изначальное",
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	e.GET("/task", GetTask)
+	e.POST("/update", UpdateTask)
+
+	err := e.Start("localhost:8080")
+	if err != nil {
+		return
+	}
+}
+
+func GetTask(e echo.Context) error {
+	return e.JSON(http.StatusOK, task)
+}
+
+func UpdateTask(e echo.Context) error {
+	var req RequestBody
+	if err := json.NewDecoder(e.Request().Body).Decode(&req); err != nil {
+		return e.JSON(http.StatusBadRequest, map[string]string{
+			"error": "invalid request body",
+		})
+	}
+	task = req
+	return e.JSON(http.StatusOK, map[string]string{
+		"status": "task updated",
+	})
+
+}
